a5447d9edfd4b3ebeca30e6c42cdcdf01e77a492
mourao9 <elvismourao9@gmail.com>
2022-01-15 11:39:23 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,15 +1,21 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "util/requests";
+import { validateEmail } from "util/validate";
 import "./styles.css";
 
 type Props = {
   movieId: string;
 };
 
+
+
 function FormCard({ movieId }: Props) {
+
+  const navigate = useNavigate();
+
   const [movie, setMovie] = useState<Movie>();
 
   useEffect(() => {
@@ -17,6 +23,30 @@ function FormCard({ movieId }: Props) {
       .get(`${BASE_URL}/movies/${movieId}`)
       .then((response) => setMovie(response.data));
   }, [movieId]);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    if(!validateEmail(email)){
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score
+      }
+    }
+    axios(config).then(res => {
+      navigate("/");
+    })
+  }
   return (
     <div className="dsmovie-form-container">
       <img
@@ -26,7 +56,7 @@ function FormCard({ movieId }: Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
